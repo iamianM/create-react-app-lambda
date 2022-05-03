0547be950c26a381d61a33089bada88cdb73dfcd
Ian McCann <mcian91@gmail.com>
2022-05-02 18:38:17 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import { useWallet } from "./components/Network/useWallet";
+import React from 'react';
 
-import CharacterEditor from "./components/CharacterEditor";
-import Footer from "./components/Footer";
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
 
 function App() {
-  const { currentAccount, setCurrentAccount } = useWallet();
   return (
     <>
-      <Router>
-        <Header
-          currentAccount={currentAccount}
-          setCurrentAccount={setCurrentAccount}
-        />
-      </Router>
       <CharacterEditor />
       <Footer />
     </>
--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -1,103 +1,26 @@
 import React from "react";
 
-import { defaultSkinColor, defaultClothesColor } from "../../constants";
+import { defaultSkinColor, defaultHairColor } from "../../constants";
 import Character from "../Character";
 import MaxWidthWrapper from "../MaxWidthWrapper";
 import ControlPane from "../ControlPane";
 
-import styles from "./CharacterEditor.module.css";
-import { skinColors, clothesColors } from "../../constants";
-import { zeroPadNumber, range } from "../../utils";
-
 import {
-  BodyCount,
-  HeadCount,
-  FaceCount,
-  AccessoryCount,
-} from "../Network/ethereum";
+  headOptions,
+  hairStyleOptions,
+  beardStyleOptions,
+  skinColorOptions,
+  hairColorOptions,
+} from "./CharacterEditor.helpers";
+import styles from "./CharacterEditor.module.css";
 
 function App() {
-  const [body, setBody] = React.useState(0);
   const [head, setHead] = React.useState(0);
-  const [face, setFace] = React.useState(0);
-  const [accessory, setAccessory] = React.useState(0);
+  const [hairStyle, setHairStyle] = React.useState(0);
+  const [beardStyle, setBeardStyle] = React.useState(0);
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
-  const [clothesColor, setClothesColor] = React.useState(defaultClothesColor);
-
-  // const [numBodies, numHeads, numFaces, numAccessories] = GetCounts();
-  const _bodyCount = BodyCount();
-  const _headCount = HeadCount();
-  const _faceCount = FaceCount();
-  const _accessoryCount = AccessoryCount();
-
-  // const __bodyCount = Promise.resolve(_bodyCount);
-  // const __headCount = Promise.resolve(_headCount);
-  // const __faceCount = Promise.resolve(_faceCount);
-  // const __accessoryCount = Promise.resolve(_accessoryCount);
-
-  const bodyCount = _bodyCount.then(function (a) {
-    return a;
-  });
-  const headCount = _headCount.then(function (a) {
-    return a;
-  });
-  const faceCount = _faceCount.then(function (a) {
-    return a;
-  });
-  const accessoryCount = _accessoryCount.then(function (a) {
-    return a;
-  });
-
-  if (bodyCount != "2") {
-    console.log("nothing", bodyCount);
-    return "";
-  }
-  console.log(accessoryCount);
-
-  const bodyOptions = range(parseInt(bodyCount)).map((index) => {
-    return {
-      id: index,
-      label: `Body ${index + 1}`,
-      children: zeroPadNumber(index + 1),
-    };
-  });
-  const headOptions = range(parseInt(headCount)).map((index) => {
-    return {
-      id: index,
-      label: `Head ${index + 1}`,
-      children: zeroPadNumber(index + 1),
-    };
-  });
-  const faceOptions = range(parseInt(faceCount)).map((index) => {
-    return {
-      id: index,
-      label: `Face ${index + 1}`,
-      children: zeroPadNumber(index + 1),
-    };
-  });
-  const accessoryOptions = range(parseInt(accessoryCount)).map((index) => {
-    return {
-      id: index,
-      label: `Accessory ${index + 1}`,
-      children: zeroPadNumber(index + 1),
-    };
-  });
-  const skinColorOptions = skinColors.map(({ label, color }) => {
-    return {
-      id: color,
-      label,
-      color,
-      children: null,
-    };
-  });
-  const clothesColorOptions = clothesColors.map(({ label, color }) => {
-    return {
-      id: color,
-      label,
-      color,
-      children: null,
-    };
-  });
+  const [hairColor, setHairColor] = React.useState(defaultHairColor);
+  console.log(skinColor);
 
   return (
     <main className={styles.characterEditor}>
@@ -112,28 +35,22 @@ function App() {
         </header>
         <div className={styles.controlColumn}>
           <ControlPane
-            title="Bodies"
-            options={bodyOptions}
-            currentOption={body}
-            handleSelectOption={setBody}
-          />
-          <ControlPane
             title="Heads"
             options={headOptions}
             currentOption={head}
             handleSelectOption={setHead}
           />
           <ControlPane
-            title="Faces"
-            options={faceOptions}
-            currentOption={face}
-            handleSelectOption={setFace}
+            title="HairStyles"
+            options={hairStyleOptions}
+            currentOption={hairStyle}
+            handleSelectOption={setHairStyle}
           />
           <ControlPane
-            title="Accessories"
-            options={accessoryOptions}
-            currentOption={accessory}
-            handleSelectOption={setAccessory}
+            title="BeardStyles"
+            options={beardStyleOptions}
+            currentOption={beardStyle}
+            handleSelectOption={setBeardStyle}
           />
           <ControlPane
             title="Skin Color"
@@ -142,22 +59,21 @@ function App() {
             handleSelectOption={setSkinColor}
           />
           <ControlPane
-            title="Clothing Color"
-            options={clothesColorOptions}
-            currentOption={clothesColor}
-            handleSelectOption={setClothesColor}
+            title="Hair Color"
+            options={hairColorOptions}
+            currentOption={hairColor}
+            handleSelectOption={setHairColor}
           />
         </div>
       </MaxWidthWrapper>
 
       <div className={styles.characterWrapper}>
         <Character
-          body={body}
           head={head}
-          face={face}
-          accessory={accessory}
+          hairStyle={hairStyle}
+          beardStyle={beardStyle}
           skinColor={skinColor}
-          clothesColor={clothesColor}
+          hairColor={hairColor}
         />
       </div>
     </main>
